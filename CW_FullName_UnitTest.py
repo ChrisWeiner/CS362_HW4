da8dd2952d855e07ed4bd8478c9c177da5ac5fd0
Christopher Weiner <chriscweiner@gmail.com>
2021-02-06 23:20:50 -0800

    changed asserts in FullName_UnitTest.py

--- CW_FullName_UnitTest.py
+++ CW_FullName_UnitTest.py
@@ -9,19 +9,19 @@ class TestCaseName(unittest.TestCase):
     def test2(self):
         self.assertEqual(FN.fullName(1,"Weiner"), "First names are not numbers")
     def test3(self):
-        self.assertEqual(FN.fullName("Chris","Weiner"),"ChrisWeiner")
+        self.assertNotEqual(FN.fullName("Chris","Weiner"),"ChrisWeiner")
     def test4(self):
-        self.assertEqual(FN.fullName("Chris","Weiner"),"Weiner Chris")
+        self.assertNotEqual(FN.fullName("Chris","Weiner"),"Weiner Chris")
     def test5(self):
-        self.assertEqual(FN.fullName("Chris","Weiner"),"Chris")
+        self.assertNotEqual(FN.fullName("Chris","Weiner"),"Chris")
     def test6(self):
-        self.assertEqual(FN.fullName("Chris","Weiner"),"Weiner")
+        self.assertNotEqual(FN.fullName("Chris","Weiner"),"Weiner")
     def test7(self):
         self.assertEqual(FN.fullName("Chris  ","  Weiner"),"Chris Weiner")
     def test8(self):
         self.assertEqual(FN.fullName("     Chris        ","          Weiner          "),"Chris Weiner")
     def test9(self):
-        self.assertEqual(FN.fullName("Weiner","Chris"),"Chris Weiner")
+        self.assertNotEqual(FN.fullName("Weiner","Chris"),"Chris Weiner")
     def test10(self):
         self.assertEqual(FN.fullName("Chris",2),"Last names are not numbers")
 if __name__ == "__main__": #Using this line from the unittest lecture to display the number of failures in the test
